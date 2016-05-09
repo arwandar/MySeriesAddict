10d7a1fbc7b5a9b164e1b4974579ab385639d4f5
Arwandar <arwandar@gmail.com>
2016-05-09 21:27:30 +0200

    finition des details d'un show

--- app/src/main/java/com/arwandar/myseriesaddict/ui/activity/ShowsDetailActivity.java
+++ app/src/main/java/com/arwandar/myseriesaddict/ui/activity/ShowsDetailActivity.java
@@ -1,22 +1,21 @@
 package com.arwandar.myseriesaddict.ui.activity;
 
 import android.app.ProgressDialog;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AlertDialog;
 import android.widget.ImageView;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.arwandar.myseriesaddict.R;
-import com.arwandar.myseriesaddict.api.SharedPrefsSingleton;
 import com.arwandar.myseriesaddict.api.converter.ShowDisplayComplexConverter;
 import com.arwandar.myseriesaddict.api.dto.ShowDisplayComplexDTO;
 import com.arwandar.myseriesaddict.api.model.Shows;
 import com.arwandar.myseriesaddict.api.service.CallManager;
 import com.squareup.picasso.Picasso;
 
+import butterknife.Bind;
+import butterknife.OnCheckedChanged;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -24,6 +23,11 @@ import retrofit2.Response;
 public class ShowsDetailActivity extends CustomActivity {
     protected ProgressDialog progress;
     Shows mShows;
+    @Bind(R.id.shows_detail_archived)
+    Switch mSwitchArchived;
+    @Bind(R.id.shows_detail_favorite)
+    Switch mSwitchFavorite;
+    private boolean alreadyArchivedCall = true, alreadyFavoriteCall = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,41 +52,128 @@ public class ShowsDetailActivity extends CustomActivity {
                     mShows = converter.convertDtoToShowDisplayComplex(response.body()).getmShow();
                     setUI();
                 } else {
-                    if (response.code() == 400) {
-                        SharedPrefsSingleton.setAccessToken("");
-                        Intent intent = new Intent(ShowsDetailActivity.this, LoginActivity.class);
-                        startActivity(intent);
-                    }
+                    showErrorLogin(response.code());
                 }
                 progress.dismiss();
             }
 
             @Override
             public void onFailure(Call<ShowDisplayComplexDTO> call, Throwable t) {
-                Toast.makeText(ShowsDetailActivity.this, "Pas d'accès à internet, veuillez réessayer plus tard.", Toast.LENGTH_SHORT).show();
                 progress.dismiss();
-                AlertDialog.Builder builder = new AlertDialog.Builder(ShowsDetailActivity.this);
-                builder.setMessage(R.string.dialog_message_error)
-                        .setTitle(R.string.dialog_title_error);
-                builder.setNeutralButton(R.string.ok_error, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        // User clicked OK button
-                    }
-                });
-                AlertDialog dialog = builder.create();
-                dialog.show();
+                showError();
             }
         });
     }
 
     public void setUI() {
         ((TextView) findViewById(R.id.shows_detail_title)).setText(mShows.getmTitle());
-        String shows_detail_pending = mShows.getmStatus() + " ( " + mShows.getmEpisodes() + " épisode(s))";
+        String shows_detail_pending = mShows.getmStatus() + " ("
+                + mShows.getmUser().getmRemaining() + " non-vu(s) épisodes sur "
+                + mShows.getmEpisodes() + ")";
         ((TextView) findViewById(R.id.shows_detail_pending)).setText(shows_detail_pending);
         ImageView image = (ImageView) findViewById(R.id.shows_detail_image);
         Picasso.with(ShowsDetailActivity.this).load(mShows.getmImages().getmShow()).into(image);
         ((TextView) findViewById(R.id.shows_detail_description)).setText(mShows.getmDescription());
 
+        mSwitchArchived.setChecked(mShows.getmUser().getmArchived().equals("true"));
+        mSwitchFavorite.setChecked(mShows.getmUser().getmFavorited().equals("true"));
+        alreadyArchivedCall = false;
+        alreadyFavoriteCall = false;
+    }
 
+    @OnCheckedChanged(R.id.shows_detail_favorite)
+    public void setFavorite() {
+        if (!alreadyFavoriteCall) {
+            alreadyFavoriteCall = true;
+            if (mSwitchFavorite.isChecked()) {
+                CallManager.markShowAsFavoriteAsync(mShows.getmId(), new Callback<ShowDisplayComplexDTO>() {
+                    @Override
+                    public void onResponse(Call<ShowDisplayComplexDTO> call, Response<ShowDisplayComplexDTO> response) {
+                        if (response.isSuccessful()) {
+                            Toast.makeText(ShowsDetailActivity.this, "La série a été marquée comme favorite.", Toast.LENGTH_SHORT).show();
+                            mSwitchFavorite.setChecked(true);
+                            alreadyFavoriteCall = false;
+                        } else {
+                            showErrorLogin(response.code());
+                            alreadyFavoriteCall = false;
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<ShowDisplayComplexDTO> call, Throwable t) {
+                        showError();
+                        alreadyFavoriteCall = false;
+                    }
+                });
+            } else {
+                CallManager.deleteShowFromFavoriteAsync(mShows.getmId(), new Callback<ShowDisplayComplexDTO>() {
+                    @Override
+                    public void onResponse(Call<ShowDisplayComplexDTO> call, Response<ShowDisplayComplexDTO> response) {
+                        if (response.isSuccessful()) {
+                            Toast.makeText(ShowsDetailActivity.this, "La série a été enlevée des favorites.", Toast.LENGTH_SHORT).show();
+                            mSwitchFavorite.setChecked(false);
+                            alreadyFavoriteCall = false;
+                        } else {
+                            showErrorLogin(response.code());
+                            alreadyFavoriteCall = false;
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<ShowDisplayComplexDTO> call, Throwable t) {
+                        showError();
+                        alreadyFavoriteCall = false;
+                    }
+                });
+            }
+        }
+    }
+
+    @OnCheckedChanged(R.id.shows_detail_archived)
+    public void setArchived() {
+        if (!alreadyArchivedCall) {
+            alreadyArchivedCall = true;
+            if (mSwitchArchived.isChecked()) {
+                CallManager.markShowAsArchivedAsync(mShows.getmId(), new Callback<ShowDisplayComplexDTO>() {
+                    @Override
+                    public void onResponse(Call<ShowDisplayComplexDTO> call, Response<ShowDisplayComplexDTO> response) {
+                        if (response.isSuccessful()) {
+                            Toast.makeText(ShowsDetailActivity.this, "La série a été marquée comme archivée.", Toast.LENGTH_SHORT).show();
+                            mSwitchArchived.setChecked(true);
+                            alreadyArchivedCall = false;
+                        } else {
+                            showErrorLogin(response.code());
+                            alreadyArchivedCall = false;
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<ShowDisplayComplexDTO> call, Throwable t) {
+                        showError();
+                        alreadyArchivedCall = false;
+                    }
+                });
+            } else {
+                CallManager.deleteShowFromArchivedAsync(mShows.getmId(), new Callback<ShowDisplayComplexDTO>() {
+                    @Override
+                    public void onResponse(Call<ShowDisplayComplexDTO> call, Response<ShowDisplayComplexDTO> response) {
+                        if (response.isSuccessful()) {
+                            Toast.makeText(ShowsDetailActivity.this, "La série a été enlevée des archivées.", Toast.LENGTH_SHORT).show();
+                            mSwitchArchived.setChecked(false);
+                            alreadyArchivedCall = false;
+                        } else {
+                            showErrorLogin(response.code());
+                            alreadyArchivedCall = false;
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(Call<ShowDisplayComplexDTO> call, Throwable t) {
+                        showError();
+                        alreadyArchivedCall = false;
+                    }
+                });
+            }
+        }
     }
 }
--- app/src/main/java/com/arwandar/myseriesaddict/ui/fragment/ShowsListFragment.java
+++ app/src/main/java/com/arwandar/myseriesaddict/ui/fragment/ShowsListFragment.java
@@ -1,26 +1,22 @@
 package com.arwandar.myseriesaddict.ui.fragment;
 
 import android.app.ProgressDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import com.arwandar.myseriesaddict.R;
-import com.arwandar.myseriesaddict.api.SharedPrefsSingleton;
 import com.arwandar.myseriesaddict.api.converter.MemberComplexConverter;
 import com.arwandar.myseriesaddict.api.dto.MemberComplexDTO;
 import com.arwandar.myseriesaddict.api.model.Shows;
 import com.arwandar.myseriesaddict.api.service.CallManager;
 import com.arwandar.myseriesaddict.ui.ItemClickSupport;
-import com.arwandar.myseriesaddict.ui.activity.LoginActivity;
+import com.arwandar.myseriesaddict.ui.activity.CustomActivity;
 import com.arwandar.myseriesaddict.ui.activity.ShowsDetailActivity;
 import com.arwandar.myseriesaddict.ui.adpater.ShowsAdapter;
 
@@ -100,27 +96,13 @@ public abstract class ShowsListFragment extends Fragment {
                     mAdapter.notifyDataSetChanged();
 
                 } else {
-                    if (response.code() == 400) {
-                        SharedPrefsSingleton.setAccessToken("");
-                        Intent intent = new Intent(getActivity(), LoginActivity.class);
-                        startActivity(intent);
-                    }
+                    ((CustomActivity) getActivity()).showErrorLogin(response.code());
                 }
             }
 
             @Override
             public void onFailure(Call<MemberComplexDTO> call, Throwable t) {
-                Toast.makeText(getActivity(), "Pas d'accès à internet, veuillez réessayer plus tard.", Toast.LENGTH_SHORT).show();
-                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
-                builder.setMessage(R.string.dialog_message_error)
-                        .setTitle(R.string.dialog_title_error);
-                builder.setNeutralButton(R.string.ok_error, new DialogInterface.OnClickListener() {
-                    public void onClick(DialogInterface dialog, int id) {
-                        // User clicked OK button
-                    }
-                });
-                AlertDialog dialog = builder.create();
-                dialog.show();
+                ((CustomActivity) getActivity()).showError();
             }
         });
     }
